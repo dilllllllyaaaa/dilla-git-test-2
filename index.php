525d03595fd264a70c6e5a6f63bfc8f0e049afae
Dilla Aprianti <Dillaapr4@gmail.com>
2021-02-08 16:03:18 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, architecto cumque. Delectus enim fuga porro nobis placeat perferendis quod architecto. Provident praesentium eligendi voluptatibus sequi, molestias suscipit quod similique eveniet.</p>
+    <ul>
+    <li>satu</li>
+    <li>dua</li>
+    </ul>
 </body>
 </html>
